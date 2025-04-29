3111000a53317f74d62c07b15729dab4173c6cf0
John Doe <bluetoothdoof@proton.me>
2025-04-29 01:35:03 -0300

    added more scaffolding for pokemon section

--- scripts/natures.js
+++ scripts/natures.js
@@ -1,30 +1,4 @@
-const NATURES = [
-    { nature: "Firme", maxConfidence: 4 },
-    { nature: "Tímida", maxConfidence: 6 },
-    { nature: "Audaciosa", maxConfidence: 9 },
-    { nature: "Valente", maxConfidence: 9 },
-    { nature: "Calma", maxConfidence: 8 },
-    { nature: "Cuidadosa", maxConfidence: 5 },
-    { nature: "Dócil", maxConfidence: 7 },
-    { nature: "Gentil", maxConfidence: 10 },
-    { nature: "Esforçada", maxConfidence: 9 },
-    { nature: "Agitada", maxConfidence: 7 },
-    { nature: "Rebelde", maxConfidence: 7 },
-    { nature: "Alegre", maxConfidence: 10 },
-    { nature: "Negligente", maxConfidence: 8 },
-    { nature: "Carente", maxConfidence: 5 },
-    { nature: "Tranquila", maxConfidence: 8 },
-    { nature: "Modesta", maxConfidence: 10 },
-    { nature: "Ingênua", maxConfidence: 7 },
-    { nature: "Sapeca", maxConfidence: 6 },
-    { nature: "Quieta", maxConfidence: 5 },
-    { nature: "Peculiar", maxConfidence: 9 },
-    { nature: "Rabugenta", maxConfidence: 6 },
-    { nature: "Relaxada", maxConfidence: 8 },
-    { nature: "Atrevida", maxConfidence: 7 },
-    { nature: "Séria", maxConfidence: 4 },
-    { nature: "Isolada", maxConfidence: 4 },
-];
+import { NATURES } from './data.js';
 
 const playerNatureSelect = document.getElementById("player-nature");
 for (const { nature } of NATURES) {
