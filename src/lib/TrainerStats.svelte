5be17454202906570148f8a8057ee9a7db57536f
John Doe <bluetoothdoof@proton.me>
2025-07-25 00:18:29 -0300

    started adding trainer stats

---
+++ src/lib/TrainerStats.svelte
@@ -0,0 +1,142 @@
+<div class="flex w-[360px] flex-col p-2">
+	<div
+		class="rounded-t-lg border border-b-0 border-solid border-zinc-950 px-4 py-2 dark:border-zinc-50"
+	>
+		Atributos
+	</div>
+	<div
+		class="flex flex-col rounded-b-lg border border-solid border-zinc-950 p-2 dark:border-zinc-50"
+	>
+		<p class="mt-0 mb-2 ml-1">Pontos a gastar: 0</p>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Força</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Destreza</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Vitalidade</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Intuição</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+
+		<hr class="my-3" />
+
+		<p class="mt-0 mb-2 ml-1">Pontos a gastar: 0</p>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Durão</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Maneiro</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Lindo</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Esperto</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+		<div class="flex">
+			<span
+				class="input-group-text min-w-[85px] rounded-l-lg border border-r-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>Fofo</span
+			>
+			<input
+				type="number"
+				class="w-full border-zinc-950 dark:border-zinc-50 dark:bg-zinc-800 dark:text-zinc-100"
+			/>
+			<span
+				class="input-group-text min-w-[50px] rounded-r-lg border border-l-0 border-zinc-950 dark:border-zinc-50 dark:bg-zinc-700"
+				>/ 5
+			</span>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import TrainerData from '$lib/TrainerData.svelte';
+	import TrainerStats from '$lib/TrainerStats.svelte';
 </script>
 
 <div
-	class="min-h-screen p-2 dark:bg-zinc-800 dark:text-zinc-100"
+	class="flex min-h-screen p-2 dark:bg-zinc-800 dark:text-zinc-100"
 	style="font-family: 'Roboto Condensed', sans-serif;"
 >
 	<TrainerData></TrainerData>
+	<TrainerStats></TrainerStats>
 </div>
